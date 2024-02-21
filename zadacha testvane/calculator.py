cde03b15905c5eb1975419bd764a0410005ba87c
Alex-Yonchev23 <125677851+Alex-Yonchev23@users.noreply.github.com>
2024-02-21 11:05:11 +0200

    Add files via upload

---
+++ zadacha testvane/calculator.py
@@ -0,0 +1,20 @@
+class Calculator:
+
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+    @staticmethod
+    def subtract(x, y):
+        return x - y
+
+    @staticmethod
+    def multiply(x, y):
+        return x * y
+
+    @staticmethod
+    def divide(x, y):
+        if y != 0:
+            return x / y
+        else:
+            raise ValueError("Cannot divide by zero.")
